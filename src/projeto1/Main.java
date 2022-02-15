453ab83b5d20a277ed5bef21c6e001d1d6abe73d
Jayan <jansantos1818@gmail.com>
2022-02-15 20:48:53 -0300

    Primeiro commit

---
+++ src/projeto1/Main.java
@@ -0,0 +1,20 @@
+package projeto1;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		
+		System.out.print("TABUADA DA MULTIPLICAÇÃO (1 a 10) \n \n");
+		
+		
+		for(int x = 1; x<11; x++) {
+			
+			for(int y = 1; y<11; y++) {
+				
+				System.out.print(x + " X " + y + " = " + x*y + "\n");
+		}
+			System.out.print("\n");
+	}
+	}
+}
